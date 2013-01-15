540aa78416bb3a26e0462749e8cff46953fe02b2
Murali <muraliavarma@gmail.com>
2013-01-15 01:46:03 -0500

    Key pressed and advance functions

--- Murali_Varma_hw1/Murali_Varma_hw1.pde
+++ Murali_Varma_hw1/Murali_Varma_hw1.pde
@@ -12,7 +12,9 @@ int CONTINUOUS_MODE = 1;
 //variables
 
 boolean [][] cellState;
+int [][] neighborCount;
 int mode;
+int neighbors;
 
 void setup() {
 	size(NUM_HORIZONTAL_CELLS * CELL_WIDTH, NUM_VERTICAL_CELLS * CELL_HEIGHT);
@@ -20,26 +22,33 @@ void setup() {
 	stroke(100);
 
 	//initialize variables
-	cellState = new boolean[NUM_VERTICAL_CELLS][NUM_HORIZONTAL_CELLS];	//by default all cells are inititialized to 0
-	mode = EDIT_MODE;
+	cellState = new boolean[NUM_VERTICAL_CELLS][NUM_HORIZONTAL_CELLS];	//by default all cells are inititialized to false
+	neighborCount = new int[NUM_VERTICAL_CELLS][NUM_HORIZONTAL_CELLS];	//by default all cells are inititialized to zero
+	mode = SINGLE_STEP_MODE;
+	drawCells();
 }
 
 void draw() {
-	drawCells();
+
 }
 
 void drawCells() {
 	for (int i = 0; i < NUM_VERTICAL_CELLS; i++) {
 		for (int j = 0; j < NUM_HORIZONTAL_CELLS; j++) {
-			if (cellState[i][j]) {
-				fill(255);
-			}
-			else {
-				fill(0);
-			}
-			rect (CELL_WIDTH * j, CELL_HEIGHT * i, CELL_WIDTH, CELL_HEIGHT);
+			drawCell(i, j);
 		}
 	}
+}
+
+void drawCell(int i, int j) {
+	if (cellState[i][j]) {
+		fill(255);
+	}
+	else {
+		fill(0);
+	}
+	rect (CELL_WIDTH * j, CELL_HEIGHT * i, CELL_WIDTH, CELL_HEIGHT);
+
 }
 
 void mousePressed() {
@@ -50,4 +59,75 @@ void mousePressed() {
 	int xCell = mouseX/CELL_WIDTH;
 	int yCell = mouseY/CELL_HEIGHT;
 	cellState[yCell][xCell] = !cellState[yCell][xCell];
+	drawCell(yCell, xCell);
+}
+
+void keyPressed() {
+	//clear grid
+	if (key == 'C' || key == 'c') {
+		for (int i = 0; i < NUM_VERTICAL_CELLS; i++) {
+			for (int j = 0; j < NUM_HORIZONTAL_CELLS; j++) {
+				cellState[i][j] = false;
+			}
+		}
+	}
+
+	//randomize grid
+	if (key == 'R' || key == 'r') {
+		for (int i = 0; i < NUM_VERTICAL_CELLS; i++) {
+			for (int j = 0; j < NUM_HORIZONTAL_CELLS; j++) {
+				//random(2) gives a float between 0 and 2
+				cellState[i][j] = (random(2) >= 1);
+			}
+		}
+	}
+
+	//change mode
+	if (key == 'g' || key == 'G') {
+		if (mode == SINGLE_STEP_MODE) {
+			mode = CONTINUOUS_MODE;
+		}
+		else {
+			mode = SINGLE_STEP_MODE;
+		}
+	}
+
+	//advance step
+	if (key == ' ') {
+		mode = SINGLE_STEP_MODE;
+		advance();
+	}
+	drawCells();
+}
+
+void advance() {
+	for (int i = 0; i < NUM_VERTICAL_CELLS; i++) {
+		for (int j = 0; j < NUM_HORIZONTAL_CELLS; j++) {
+			neighbors = 0;
+			for (int ii = -1; ii <= 1; ii++) {
+				for (int jj = -1; jj <= 1; jj++) {
+					if ( ii == 0 && jj == 0) {
+						continue;
+					}
+					int newX = (j + jj + NUM_HORIZONTAL_CELLS) % NUM_HORIZONTAL_CELLS;
+					int newY = (i + ii + NUM_VERTICAL_CELLS) % NUM_VERTICAL_CELLS;
+					if (cellState[newY][newX]) {
+						neighbors ++;
+					}
+				}
+			}
+			neighborCount[i][j] = neighbors;
+		}
+	}
+
+	for (int i = 0; i < NUM_VERTICAL_CELLS; i++) {
+		for (int j = 0; j < NUM_HORIZONTAL_CELLS; j++) {
+			if (cellState[i][j] && neighborCount[i][j] != 2 && neighborCount[i][j] != 3) {
+				cellState[i][j] = false;
+			}
+			else if (!cellState[i][j] && neighborCount[i][j] == 3) {
+				cellState[i][j] = true;
+			}
+		}
+	}
 }
--- Murali_Varma_hw1/build-tmp/source/Murali_Varma_hw1.java
+++ Murali_Varma_hw1/build-tmp/source/Murali_Varma_hw1.java
@@ -21,14 +21,15 @@ int NUM_VERTICAL_CELLS = 50;
 int CELL_HEIGHT = 8;
 int CELL_WIDTH = 8;
 
-int EDIT_MODE = 0;
-int SINGLE_STEP_MODE = 1;
-int CONTINUOUS_MODE = 2;
+int SINGLE_STEP_MODE = 0;
+int CONTINUOUS_MODE = 1;
 
 //variables
 
 boolean [][] cellState;
+int [][] neighborCount;
 int mode;
+int neighbors;
 
 public void setup() {
 	size(NUM_HORIZONTAL_CELLS * CELL_WIDTH, NUM_VERTICAL_CELLS * CELL_HEIGHT);
@@ -36,36 +37,114 @@ public void setup() {
 	stroke(100);
 
 	//initialize variables
-	cellState = new boolean[NUM_VERTICAL_CELLS][NUM_HORIZONTAL_CELLS];	//by default all cells are inititialized to 0
-	mode = EDIT_MODE;
+	cellState = new boolean[NUM_VERTICAL_CELLS][NUM_HORIZONTAL_CELLS];	//by default all cells are inititialized to false
+	neighborCount = new int[NUM_VERTICAL_CELLS][NUM_HORIZONTAL_CELLS];	//by default all cells are inititialized to zero
+	mode = SINGLE_STEP_MODE;
+	drawCells();
 }
 
 public void draw() {
-	drawCells();
+
 }
 
 public void drawCells() {
 	for (int i = 0; i < NUM_VERTICAL_CELLS; i++) {
 		for (int j = 0; j < NUM_HORIZONTAL_CELLS; j++) {
-			if (cellState[i][j]) {
-				fill(255);
-			}
-			else {
-				fill(0);
-			}
-			rect (CELL_WIDTH * j, CELL_HEIGHT * i, CELL_WIDTH, CELL_HEIGHT);
+			drawCell(i, j);
 		}
 	}
 }
 
+public void drawCell(int i, int j) {
+	if (cellState[i][j]) {
+		fill(255);
+	}
+	else {
+		fill(0);
+	}
+	rect (CELL_WIDTH * j, CELL_HEIGHT * i, CELL_WIDTH, CELL_HEIGHT);
+
+}
+
 public void mousePressed() {
-	if (mode != EDIT_MODE) {
-		//mouse clicks have effect only in edit mode
+	if (mode != SINGLE_STEP_MODE) {
+		//mouse clicks have effect only in single step mode
 		return;
 	}
 	int xCell = mouseX/CELL_WIDTH;
 	int yCell = mouseY/CELL_HEIGHT;
 	cellState[yCell][xCell] = !cellState[yCell][xCell];
+	drawCell(yCell, xCell);
+}
+
+public void keyPressed() {
+	//clear grid
+	if (key == 'C' || key == 'c') {
+		for (int i = 0; i < NUM_VERTICAL_CELLS; i++) {
+			for (int j = 0; j < NUM_HORIZONTAL_CELLS; j++) {
+				cellState[i][j] = false;
+			}
+		}
+	}
+
+	//randomize grid
+	if (key == 'R' || key == 'r') {
+		for (int i = 0; i < NUM_VERTICAL_CELLS; i++) {
+			for (int j = 0; j < NUM_HORIZONTAL_CELLS; j++) {
+				//random(2) gives a float between 0 and 2
+				cellState[i][j] = (random(2) >= 1);
+			}
+		}
+	}
+
+	//change mode
+	if (key == 'g' || key == 'G') {
+		if (mode == SINGLE_STEP_MODE) {
+			mode = CONTINUOUS_MODE;
+		}
+		else {
+			mode = SINGLE_STEP_MODE;
+		}
+	}
+
+	//advance step
+	if (key == ' ') {
+		mode = SINGLE_STEP_MODE;
+		advance();
+	}
+	drawCells();
+}
+
+public void advance() {
+	for (int i = 0; i < NUM_VERTICAL_CELLS; i++) {
+		for (int j = 0; j < NUM_HORIZONTAL_CELLS; j++) {
+			neighbors = 0;
+			for (int ii = -1; ii <= 1; ii++) {
+				for (int jj = -1; jj <= 1; jj++) {
+					if ( ii == 0 && jj == 0) {
+						continue;
+					}
+					int newX = (j + jj + NUM_HORIZONTAL_CELLS) % NUM_HORIZONTAL_CELLS;
+					int newY = (i + ii + NUM_VERTICAL_CELLS) % NUM_VERTICAL_CELLS;
+					if (cellState[newY][newX]) {
+						neighbors ++;
+					}
+				}
+			}
+			neighborCount[i][j] = neighbors;
+		}
+	}
+
+	for (int i = 0; i < NUM_VERTICAL_CELLS; i++) {
+		for (int j = 0; j < NUM_HORIZONTAL_CELLS; j++) {
+			if (cellState[i][j] && neighborCount[i][j] != 2 && neighborCount[i][j] != 3) {
+				cellState[i][j] = false;
+			}
+			else if (!cellState[i][j] && neighborCount[i][j] == 3) {
+				cellState[i][j] = true;
+			}
+		}
+	}
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Murali_Varma_hw1" };
