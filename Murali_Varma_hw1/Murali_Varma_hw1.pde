b55d53731670f9cfca94aa54d7ad4ef285877473
Murali <muraliavarma@gmail.com>
2013-01-15 00:08:07 -0500

    Added mouse pressed function

--- Murali_Varma_hw1/Murali_Varma_hw1.pde
+++ Murali_Varma_hw1/Murali_Varma_hw1.pde
@@ -6,22 +6,21 @@ int NUM_VERTICAL_CELLS = 50;
 int CELL_HEIGHT = 8;
 int CELL_WIDTH = 8;
 
-int EDIT_MODE = 0;
-int SINGLE_STEP_MODE = 1;
-int CONTINUOUS_MODE = 2;
+int SINGLE_STEP_MODE = 0;
+int CONTINUOUS_MODE = 1;
 
 //variables
 
-int[][] cellState;
+boolean [][] cellState;
 int mode;
 
 void setup() {
 	size(NUM_HORIZONTAL_CELLS * CELL_WIDTH, NUM_VERTICAL_CELLS * CELL_HEIGHT);
 	background(0);
+	stroke(100);
 
 	//initialize variables
-
-	cellState = new int[NUM_VERTICAL_CELLS][NUM_HORIZONTAL_CELLS];	//by default all cells are inititialized to 0
+	cellState = new boolean[NUM_VERTICAL_CELLS][NUM_HORIZONTAL_CELLS];	//by default all cells are inititialized to 0
 	mode = EDIT_MODE;
 }
 
@@ -32,15 +31,23 @@ void draw() {
 void drawCells() {
 	for (int i = 0; i < NUM_VERTICAL_CELLS; i++) {
 		for (int j = 0; j < NUM_HORIZONTAL_CELLS; j++) {
+			if (cellState[i][j]) {
+				fill(255);
+			}
+			else {
+				fill(0);
+			}
 			rect (CELL_WIDTH * j, CELL_HEIGHT * i, CELL_WIDTH, CELL_HEIGHT);
 		}
 	}
 }
 
 void mousePressed() {
-	if (mode != EDIT_MODE) {
-		//mouse clicks have effect only in edit mode
+	if (mode != SINGLE_STEP_MODE) {
+		//mouse clicks have effect only in single step mode
 		return;
 	}
-	
+	int xCell = mouseX/CELL_WIDTH;
+	int yCell = mouseY/CELL_HEIGHT;
+	cellState[yCell][xCell] = !cellState[yCell][xCell];
 }
--- Murali_Varma_hw1/build-tmp/source/Murali_Varma_hw1.java
+++ Murali_Varma_hw1/build-tmp/source/Murali_Varma_hw1.java
@@ -21,14 +21,23 @@ int NUM_VERTICAL_CELLS = 50;
 int CELL_HEIGHT = 8;
 int CELL_WIDTH = 8;
 
+int EDIT_MODE = 0;
+int SINGLE_STEP_MODE = 1;
+int CONTINUOUS_MODE = 2;
+
 //variables
 
-int[][] cellState;
+boolean [][] cellState;
+int mode;
 
 public void setup() {
 	size(NUM_HORIZONTAL_CELLS * CELL_WIDTH, NUM_VERTICAL_CELLS * CELL_HEIGHT);
 	background(0);
-	cellState = new int[NUM_VERTICAL_CELLS][NUM_HORIZONTAL_CELLS];	//by default all cells are inititialized to 0
+	stroke(100);
+
+	//initialize variables
+	cellState = new boolean[NUM_VERTICAL_CELLS][NUM_HORIZONTAL_CELLS];	//by default all cells are inititialized to 0
+	mode = EDIT_MODE;
 }
 
 public void draw() {
@@ -38,9 +47,25 @@ public void draw() {
 public void drawCells() {
 	for (int i = 0; i < NUM_VERTICAL_CELLS; i++) {
 		for (int j = 0; j < NUM_HORIZONTAL_CELLS; j++) {
+			if (cellState[i][j]) {
+				fill(255);
+			}
+			else {
+				fill(0);
+			}
 			rect (CELL_WIDTH * j, CELL_HEIGHT * i, CELL_WIDTH, CELL_HEIGHT);
 		}
 	}
+}
+
+public void mousePressed() {
+	if (mode != EDIT_MODE) {
+		//mouse clicks have effect only in edit mode
+		return;
+	}
+	int xCell = mouseX/CELL_WIDTH;
+	int yCell = mouseY/CELL_HEIGHT;
+	cellState[yCell][xCell] = !cellState[yCell][xCell];
 }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Murali_Varma_hw1" };
